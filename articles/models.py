ac8096bbb1846ff52761c2740285179e5dafd4bc
Felipe Bailez <bailez@usp.br>
2020-04-20 18:16:40 -0300

    models ajusts articles

--- articles/models.py
+++ articles/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
-# Create your models here.
-class Reporter:
-    full_name = models.CharField(max_lenght=100)
-    
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
     def __str__(self):
         return self.full_name
 
 class Article(models.Model):
     pub_date = models.DateField()
-    headline = models.CharField(max_lenght=200)
+    headline = models.CharField(max_length=200)
     content = models.TextField()
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
 
