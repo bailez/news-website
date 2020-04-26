08afe2ab0ea4959c52c76dd2657ca11f8d406548
Felipe Bailez <bailez@usp.br>
2020-04-26 01:13:57 -0300

    added views functions and html

--- news_website/articles/views.py
+++ news_website/articles/views.py
@@ -1,5 +1,14 @@
+import django
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
+
+django.setup()
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    context = {'year' : year,'article_list' : a_list}
+    return render(request, 'news/year_archive.html', context)
