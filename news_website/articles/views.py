80ca092757cedbfc6b418916f4708b88b6c04a11
Felipe Bailez <bailez@usp.br>
2020-04-27 10:59:45 -0300

    added reporter tab to views

--- news_website/articles/views.py
+++ news_website/articles/views.py
@@ -9,3 +9,8 @@ def year_archive(request, year):
     a_list = Article.objects.filter(pub_date__year=year)
     context = {'year' : year,'article_list' : a_list}
     return render(request, 'news/year_archive.html', context)
+
+def reporter(request, report):
+    a_list = Article.objects.filter(reporter=report)
+    context = {'reporter' : report, 'article_list' : a_list}
+    return rendre(request, 'news/reporter.html',context)
