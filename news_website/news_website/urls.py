7219b571096ede02f8090e93885f4fb062772563
Felipe Bailez <bailez@usp.br>
2020-05-04 12:38:45 -0300

    setup registration

--- news_website/news_website/urls.py
+++ news_website/news_website/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from register import views as regviews
 from articles import views
 
 urlpatterns = [
-    path('articles/', views.articles),
+    path('articles/', views.articles, name = 'articles'),
     path('articles/<int:year>/', views.year_archive),
     path('articles/reporter/<int:report>/', views.reporter),
+    path('register/', regviews.register, name='register'),
     path('admin/', admin.site.urls),
 ]
